1bb47e34df16a3845c0f858fcf41f1862f0d47bf
reactuzx <reactuz@mail.ru>
2019-06-01 21:26:05 +0500

    refactoring: converted stateless stuff into dumb components

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,38 +3,31 @@ import { connect } from "react-redux";
 import { handleClick } from "../actions.js";
 import Square from "./Square.js";
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        handleClick={() => this.props.handleClick(i)}
-      />
-    );
-  }
+const Board = props => {
+  const renderSquare = i => (
+    <Square value={props.squares[i]} handleClick={() => props.handleClick(i)} />
+  );
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   squares: state.history[state.stepNumber].squares
--- src/components/Game.js
+++ src/components/Game.js
@@ -4,41 +4,39 @@ import { jumpTo } from "../actions.js";
 import { connect } from "react-redux";
 import calculateWinner from "../helpers/calculateWinner.js";
 
-class Game extends React.Component {
-  render() {
-    const history = this.props.history;
-    const current = history[this.props.stepNumber];
-    const winner = calculateWinner(current.squares);
+const Game = props => {
+  const history = props.history;
+  const current = history[props.stepNumber];
+  const winner = calculateWinner(current.squares);
 
-    const moves = history.map((step, move) => {
-      const desc = move ? `Go to move #${move}` : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.props.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.props.xIsNext ? "X" : "O");
-    }
-
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : "Go to game start";
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
+      <li key={move}>
+        <button onClick={() => props.jumpTo(move)}>{desc}</button>
+      </li>
     );
+  });
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (props.xIsNext ? "X" : "O");
   }
-}
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   history: state.history,
